ad075852c528759b6d0f04e603d35c878ed06f1e
George Aziz <123029529+GeorgeAyy@users.noreply.github.com>
2024-08-03 20:10:20 +0300

    added 404

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,14 @@ const indexRoutes = require("./indexRoutes");
 function setupRoutes(app) {
   // Initialize your routes here...
   app.use("/", indexRoutes);
+  // set up 404
+
+  app.use((req, res) => {
+    res.render("404", {
+      currentPage: "404",
+      user: req.session.user === undefined ? "" : req.session.user,
+    });
+  });
 }
 
 module.exports = { setupRoutes };
